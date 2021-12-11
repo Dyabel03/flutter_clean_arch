96e2c3d02bb8c259c7bfc76b630e958b79bffb7f
Andres Felipe Chaparro <andresfelipecg@gmail.com>
2021-12-11 16:07:15 -0500

    infraestructure

--- lib/ui/common/molecules/our_header.dart
+++ lib/ui/common/molecules/our_header.dart
@@ -1,23 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_arch/ui/common/tokens/colors.dart';
 import 'package:flutter_clean_arch/ui/common/atoms/our_text.dart';
 import 'package:flutter_clean_arch/ui/common/tokens/typography.dart';
+
 class OurAppBar extends StatelessWidget with PreferredSizeWidget {
   final String title;
   final Icon iconAppBarr;
 
-  OurAppBar({
-    this.title = 'Arquitectura limpia',
-    this.iconAppBarr = const Icon(Icons.add)
-  });
+  OurAppBar(
+      {this.title = 'Arquitectura limpia',
+      this.iconAppBarr = const Icon(Icons.add)});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: SCALE_05,
-      title: Text(this.title,key: Key('app-bar-title'),
-        style: TextStyle(fontSize: H1_SIZE),),
+      title: Text(
+        this.title,
+        key: Key('app-bar-title'),
+        style: TextStyle(fontSize: H1_SIZE),
+      ),
       actions: [
         IconButton(
           icon: this.iconAppBarr,
@@ -34,6 +36,4 @@ class OurAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   // TODO: implement preferredSize
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
-
-
 }
